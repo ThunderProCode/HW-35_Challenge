cced7fcf95a1e42294f6e1cc8fb3668766fcd68a
thunderprocode <hectora887ju@gmail.com>
2023-07-24 22:30:44 -0600

    Challenge done

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static int RandomlyRecreate(string word)
+    {
+        Random random = new Random();
+        int attempts = 0;
+        string recreatedWord = "";
+
+        while (recreatedWord != word)
+        {
+            recreatedWord = GenerateRandomWord(word.Length, random);
+            attempts++;
+        }
+
+        return attempts;
+    }
+
+    public static Task<int> RandomlyRecreateAsync(string word)
+    {
+        return Task.Run(() => RandomlyRecreate(word));
+    }
+
+    private static string GenerateRandomWord(int length, Random random)
+    {
+        char[] wordChars = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            wordChars[i] = (char)('a' + random.Next(26));
+        }
+
+        return new string(wordChars);
+    }
+
+    public static async Task Main()
+    {
+        Console.WriteLine("Enter a word: ");
+        string word = Console.ReadLine();
+
+        DateTime startTime = DateTime.Now;
+        int attempts = await RandomlyRecreateAsync(word);
+        DateTime endTime = DateTime.Now;
+
+        Console.WriteLine($"It took {attempts} attempts to randomly recreate the word \"{word}\".");
+        Console.WriteLine($"Time Elapsed: {endTime - startTime}");
+    }
+}
